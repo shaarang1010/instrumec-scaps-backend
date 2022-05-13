79238025a368d10bfa51c6f18318768fdcba4847
Shaarang Tanpure <shaarang.1910@gmail.com>
2022-05-13 23:21:01 +1000

    add python mock server to mock SCAPS server

---
+++ mock_server/src/server/socket_server.py
@@ -0,0 +1,19 @@
+# echo-server.py
+
+import socket
+
+HOST = "0.0.0.0"
+PORT = 4000
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(data.decode("utf-8"))
+            conn.sendall(data)
