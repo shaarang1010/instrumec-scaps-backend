50f5066a737b8750c390f8dfc4559a2042ae3b66
Shaarang Tanpure <shaarang.1910@gmail.com>
2022-05-15 21:25:16 +1000

    add updated hopper count to cache

--- middleware/helpers/hopperMapper.js
+++ middleware/helpers/hopperMapper.js
@@ -1,24 +1,38 @@
 const { hoppers } = require("../../setup/hoppers.json");
 const lodash = require("lodash");
-const { getFromCache } = require("./cacheCommands");
+const { getFromCache, setToCache } = require("./cacheCommands");
 
 const maptoHopper = (hopperNumber) => {
   const hopperCount = getFromCache("hopperCount");
+  let hopper = "";
+  let index = 0;
   if (hopperCount[hopperNumber - 1] > 5) {
-    return hopperNumber;
+    index = hopperNumber - 1;
+    hopper = hopperNumber;
   } else if (hopperCount[hopperNumber - 1] === 0 || hopperCount[hopperNumber - 1] <= 5) {
     console.log("Empty Hopper ------ ");
     let alternativeHoppers = { ...hoppers };
     const selectedColor = lodash.get(alternativeHoppers, `H${hopperNumber}`);
+    console.log("Selected Color ==== ", selectedColor);
     delete alternativeHoppers[`H${hopperNumber}`];
     const alternateColor = Object.keys(alternativeHoppers).find((key) => alternativeHoppers[key] === selectedColor);
-    console.log("alternateColor", alternateColor);
-    return alternateColor.replace("H", "");
+    console.log("alternateColor ===== ", alternateColor);
+    index = Object.keys(alternativeHoppers).indexOf(alternateColor) + 1;
+    hopper = alternateColor.replace("H", "");
   }
+  updateHopperCount(hopperCount, index);
+  return hopper;
 };
 
 const setHopperCount = (count) => {
   return count;
+};
+
+const updateHopperCount = (hopperCount, index) => {
+  const currentCount = [...hopperCount];
+  currentCount[index] = currentCount[index] - 1;
+  console.log("Setting Hoppers to =====", currentCount);
+  setToCache("hopperCount", currentCount);
 };
 
 const formatHopperCount = (hopperCount) => {
